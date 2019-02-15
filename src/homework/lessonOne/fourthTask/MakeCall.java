99236d1042ddd89653474a1053973fcbfd123060
Oleksandr Samsonov <oleksandr_samsonov@epam.com>
2019-02-15 10:22:55 +0200

    Added an incoming call method in the fourth task

--- src/homework/lessonOne/fourthTask/MakeCall.java
+++ src/homework/lessonOne/fourthTask/MakeCall.java
@@ -17,13 +17,13 @@ public class MakeCall {
         Attempt to call the number without registering with the network
         * In this case must return message "The number you dialed is not registered on the network!"
         * */
-        System.out.println(phoneOne.phoneCall(phoneTwo.getPhoneNumber()) + "\n");
+        System.out.println(phoneOne.phoneCall(phoneTwo) + "\n");
 
         /*
         * Attempting to call from a non-registered number
         * In this case must return message "Your number is not registered on the network!"
         * */
-        System.out.println(phoneTwo.phoneCall(phoneOne.getPhoneNumber()) + "\n");
+        System.out.println(phoneTwo.phoneCall(phoneOne) + "\n");
 
         /*
         * Register phoneTwo in the network. Must return true
@@ -32,20 +32,22 @@ public class MakeCall {
 
         /*
         * Call from phoneOne to phoneTwo
-        * In this case must return message "Call in progress..."
+        * In this case must return messages "Call in progress..."
+        * and "Phone +3954897486 you have an incoming call from +3212345"
         * */
-        System.out.println(phoneOne.phoneCall(phoneTwo.getPhoneNumber()) + "\n");
+        System.out.println(phoneOne.phoneCall(phoneTwo) + "\n");
 
         /*
         * Call from phoneTwo to phoneOne
-        * In this case must return message "Call in progress..."
+        * In this case must return messages "Call in progress..."
+        * and "Phone +3212345 you have an incoming call from +3954897486"
         * */
-        System.out.println(phoneTwo.phoneCall(phoneOne.getPhoneNumber()) + "\n");
+        System.out.println(phoneTwo.phoneCall(phoneOne) + "\n");
 
         /*
         * Attempt to call yourself
         * In this case must return message "You can't call yourself :("
         * */
-        System.out.println(phoneOne.phoneCall(phoneOne.getPhoneNumber()));
+        System.out.println(phoneOne.phoneCall(phoneOne));
     }
 }
--- src/homework/lessonOne/fourthTask/Phone.java
+++ src/homework/lessonOne/fourthTask/Phone.java
@@ -49,25 +49,32 @@ public class Phone {
     * In case of its absence, displays the appropriate message.
     * Prevents attempts to call yourself
     * */
-    public String phoneCall(String anotherNumber) {
+    public String phoneCall(Phone phone) {
 
         if (!registration) {
 
             return "Your number is not registered on the network!";
 
-        } else if(phoneNumber.equals(anotherNumber)) {
+        } else if(phoneNumber.equals(phone.getPhoneNumber())) {
 
             return "You can't call yourself :(";
 
         } else {
 
-            if (Network.baseOfTelephoneNumbers.contains(anotherNumber)) {
+            if (Network.baseOfTelephoneNumbers.contains(phone.getPhoneNumber())) {
 
-                return "Call in progress...";
+                return "Call in progress..." + "\n" + phone.incomingCall(phoneNumber) + "\n";
+
             } else {
 
                 return "The number you dialed is not registered on the network!";
             }
         }
+    }
+
+    public String incomingCall(String nomber) {
+
+        return "Phone " + phoneNumber + " you have an incoming call from " + nomber;
+
     }
 }
